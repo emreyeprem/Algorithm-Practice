6645614aef1dc9a64649a4ffcc4ec9a33f9861d0
Emre Akurek <emreyeprem@gmail.com>
2018-12-22 22:07:04 -0600

    practice

--- index.js
+++ index.js
@@ -183,3 +183,18 @@ function isFizzBuzz(number){
   }
 }
 console.log(isFizzBuzz(120))
+// -------------- Pallindrome ------------------
+
+#third trial
+number = int(input("Enter a number: "))
+def isPrime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, n, 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+print(isPrime(number))      
