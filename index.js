5a8466c107b860cc485727428b2eb2dbb7a8ab74
Emre Akurek <emreyeprem@gmail.com>
2018-12-28 16:40:13 -0600

    continue learning

--- index.js
+++ index.js
@@ -217,3 +217,80 @@ function String(string){
     return 'not ' + string;
   }
 }
+
+// -------- Combining .map(), .reduce(), and .filter() ---------
+var personnel = [
+  {
+    id: 5,
+    name: "Luke Skywalker",
+    pilotingScore: 98,
+    shootingScore: 56,
+    isForceUser: true,
+  },
+  {
+    id: 82,
+    name: "Sabine Wren",
+    pilotingScore: 73,
+    shootingScore: 99,
+    isForceUser: false,
+  },
+  {
+    id: 22,
+    name: "Zeb Orellios",
+    pilotingScore: 20,
+    shootingScore: 59,
+    isForceUser: false,
+  },
+  {
+    id: 15,
+    name: "Ezra Bridger",
+    pilotingScore: 43,
+    shootingScore: 67,
+    isForceUser: true,
+  },
+  {
+    id: 11,
+    name: "Caleb Dume",
+    pilotingScore: 71,
+    shootingScore: 85,
+    isForceUser: true,
+  },
+];
+
+//Our objective: get the total score of force users only. Let’s do it step by step!
+//First, we need to filter out the personnel who can’t use the force:
+
+var jediPersonnel = personnel.filter(function (person) {
+  return person.isForceUser;
+});
+// Result: [{...}, {...}, {...}] (Luke, Ezra and Caleb)
+//With that we have 3 elements left in our resulting array. We now need to create an array containing the total score of each Jedi.
+
+var jediScores = jediPersonnel.map(function (jedi) {
+  return jedi.pilotingScore + jedi.shootingScore;
+});
+// Result: [154, 110, 156]
+//And let’s use reduce to get the total:
+
+var totalJediScore = jediScores.reduce(function (acc, score) {
+  return acc + score;
+}, 0);
+// Result: 420
+//And now here’s the fun part… we can chain all of this to get what we want in a single line:
+
+var totalJediScore = personnel
+  .filter(function (person) {
+    return person.isForceUser;
+  })
+  .map(function (jedi) {
+    return jedi.pilotingScore + jedi.shootingScore;
+  })
+  .reduce(function (acc, score) {
+    return acc + score;
+  }, 0);
+//And look how pretty it is with arrow functions:
+
+const totalJediScore = personnel
+  .filter(person => person.isForceUser)
+  .map(jedi => jedi.pilotingScore + jedi.shootingScore)
+  .reduce((acc, score) => acc + score, 0);
