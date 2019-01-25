5e12f77f9738db79bf3b76ebbf7df401efd6f32b
Emre Akurek <emreyeprem@gmail.com>
2019-01-25 16:18:29 -0600

    practice

--- index.js
+++ index.js
@@ -571,3 +571,25 @@ public boolean array123(int[] nums) {
   }
   return false;
 }
+
+// -------------------- array 667 ----------------------------
+
+// Given an array of ints, return the number of times that two 6 s are next to each other in the array. Also count instances where the second "6" is actually a 7.
+//
+//
+// array667([6, 6, 2]) → 1
+// array667([6, 6, 2, 6]) → 1
+// array667([6, 7, 2, 6]) → 1
+
+public int array667(int[] nums) {
+  int count = 0;
+  // Note: iterate to length-1, so can use i+1 in the loop
+  for (int i=0; i < (nums.length-1); i++) {
+    if (nums[i] == 6) {
+      if (nums[i+1] == 6 || nums[i+1] == 7) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
