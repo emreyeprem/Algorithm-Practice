3bd0311545b6ebaaaa8a1273ac97448a27d9958c
Emre Akurek <emreyeprem@gmail.com>
2019-01-24 22:44:05 -0600

    practice

--- index.js
+++ index.js
@@ -555,3 +555,19 @@ public String altPairs(String str) {
 public boolean or35(int n) {
   return (n % 3 == 0) || (n % 5 == 0);
 }
+
+// -------------------- array 123 ---------------------------
+
+// Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+//
+// array123([1, 1, 2, 3, 1]) → true
+// array123([1, 1, 2, 4, 1]) → false
+// array123([1, 1, 2, 1, 2, 3]) → true
+
+public boolean array123(int[] nums) {
+  // Note: iterate < length-2, so can use i+1 and i+2 in the loop
+  for (int i=0; i < (nums.length-2); i++) {
+    if (nums[i]==1 && nums[i+1]==2 && nums[i+2]==3) return true;
+  }
+  return false;
+}
