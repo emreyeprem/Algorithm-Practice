b6299c5584ad9c82307e87f115e4b36e10da8dbb
Emre Akurek <emreyeprem@gmail.com>
2019-01-27 15:31:23 -0600

    practice

--- index.js
+++ index.js
@@ -615,3 +615,19 @@ public String stringYak(String str) {
 
   return result;
 }
+
+// --------------------- string Splosion ----------------
+// Given a non-empty string like "Code" return a string like "CCoCodCode".
+//
+// stringSplosion("Code") → "CCoCodCode"
+// stringSplosion("abc") → "aababc"
+// stringSplosion("ab") → "aab"
+
+public String stringSplosion(String str) {
+  String result = "";
+  // On each iteration, add the substring of the chars 0..i
+  for (int i=0; i<str.length(); i++) {
+    result = result + str.substring(0, i+1);
+  }
+  return result;
+}
