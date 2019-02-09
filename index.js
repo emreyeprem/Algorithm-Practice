c5df2f99deb84f1191b9eae0d11eff3531d19987
Emre Akurek <emreyeprem@gmail.com>
2019-02-08 18:56:34 -0600

    practice

--- index.js
+++ index.js
@@ -666,3 +666,22 @@ public String stringBits(String str) {
   }
   return result;
 }
+
+
+//Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+// sumDouble(1, 2) → 3
+// sumDouble(3, 2) → 5
+// sumDouble(2, 2) → 8
+
+public int sumDouble(int a, int b) {
+  // Store the sum in a local variable
+  int sum = a + b;
+  
+  // Double it if a and b are the same
+  if (a == b) {
+    sum = sum * 2;
+  }
+  
+  return sum;
+}
