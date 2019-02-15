6b39f27189ce23dec48cbdda194c740a15f60e3e
Emre Akurek <emreyeprem@gmail.com>
2019-02-14 22:22:36 -0600

    practice

--- index.js
+++ index.js
@@ -783,3 +783,34 @@ function findLargestDifference(array) {
 
   return currentMaxDifference;
 }
+
+//Find the intersection of two arrays. An intersection would be the common elements that exists within both arrays. In this case, these elements should be unique!
+var firstArray = [2, 2, 4, 1];
+var secondArray = [1, 2, 0, 2];
+
+intersection(firstArray, secondArray); // [2, 1]
+
+function intersection(firstArray, secondArray) {
+  // The logic here is to create a hashmap with the elements of the firstArray as the keys.
+  // After that, you can use the hashmap's O(1) look up time to check if the element exists in the hash
+  // If it does exist, add that element to the new array.
+
+  var hashmap = {};
+  var intersectionArray = [];
+
+  firstArray.forEach(function(element) {
+    hashmap[element] = 1;
+  });
+
+  // Since we only want to push unique elements in our case... we can implement a counter to keep track of what we already added
+  secondArray.forEach(function(element) {
+    if (hashmap[element] === 1) {
+      intersectionArray.push(element);
+      hashmap[element]++;
+    }
+  });
+
+  return intersectionArray;
+
+  // Time complexity O(n), Space complexity O(n)
+}
