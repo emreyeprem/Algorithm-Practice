551a52f71c4950f3941f346ef186ab9f20babd60
Emre Akurek <emreyeprem@gmail.com>
2019-02-19 14:01:57 -0600

    practice

--- index.js
+++ index.js
@@ -890,5 +890,22 @@ function fibonacci(n){
   }
 }
 console.log(fibonacci(12))  //144
-// -------------------------------------------------
+// ------------------- Greatest Common Divisor -------------------
+function greatestCommonDivisor(a, b){
+  var divisor = 2, 
+      greatestDivisor = 1;
+
+  //if u pass a -ve number this will not work. fix it dude!!
+  if (a < 2 || b < 2)
+     return 1;
+  
+  while(a >= divisor && b >= divisor){
+   if(a %divisor == 0 && b% divisor ==0){
+      greatestDivisor = divisor;      
+    }
+   divisor++;
+  }
+  return greatestDivisor;
+}
+console.log(greatestCommonDivisor(69, 169))
 
