3d35a16c01dfb0cdd30763fbab1fb4ec0064845b
Emre Akurek <emreyeprem@gmail.com>
2019-02-22 20:33:37 -0600

    practice

--- index.js
+++ index.js
@@ -909,3 +909,33 @@ function greatestCommonDivisor(a, b){
 }
 console.log(greatestCommonDivisor(69, 169))
 
+// -------- Convert military time to standard time in javascript ------
+
+
+var time = "16:30:00"; // your input
+
+time = time.split(':'); // convert to array
+
+// fetch
+var hours = Number(time[0]);
+var minutes = Number(time[1]);
+var seconds = Number(time[2]);
+
+// calculate
+var timeValue;
+
+if (hours > 0 && hours <= 12) {
+  timeValue= "" + hours;
+} else if (hours > 12) {
+  timeValue= "" + (hours - 12);
+} else if (hours == 0) {
+  timeValue= "12";
+}
+ 
+timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+// show
+alert(timeValue);
+console.log(timeValue);
