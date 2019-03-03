3708ae98113770b90a37f6bf78951de8d0bf620c
Emre Akurek <emreyeprem@gmail.com>
2019-03-03 17:10:28 -0600

    practice

--- index.js
+++ index.js
@@ -1023,3 +1023,17 @@ function duplicateNumber(arr) {
 
 console.log(duplicateNumber([3, 5, 6, 8, 5, 3]));
 // ---------------------------------
+function duplicateNumber(arr) {
+  var memory = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(memory[arr[i]] === undefined){
+      memory[arr[i]] = true;
+    }else{
+      return arr[i]
+    }
+  }
+}
+
+var arr = [3, 5, 6, 8, 5, 3];
+console.log(duplicateNumber(arr));
+// ----------------------------------
