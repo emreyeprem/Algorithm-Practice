6d231038f156a4b689360e3a8cd811e7a80d44fe
Emre Akurek <emreyeprem@gmail.com>
2019-03-28 14:04:29 -0500

    practice

--- index.js
+++ index.js
@@ -1349,4 +1349,29 @@ function reverse(str){
   >firstNonRepeatChar('the quick brown fox jumps then quickly blow air');
    = "f"
 
-  //  *****************************
+  //  ************ Remove Duplicate Character *****************
+
+  function removeDuplicateChar(str){
+    var len = str.length,
+        char, 
+        charCount = {}, 
+        newStr = [];
+    for(var i =0; i<len; i++){
+      char = str[i];
+      if(charCount[char]){
+        charCount[char]++;
+      }
+      else
+        charCount[char] = 1;
+    }
+    for (var j in charCount){
+      if (charCount[j]==1)
+         newStr.push(j);
+    }
+    return newStr.join('');
+  }
+  
+  > removeDuplicateChar('Learn more javascript dude');
+    = "Lnmojvsciptu"
+
+    // **************************
