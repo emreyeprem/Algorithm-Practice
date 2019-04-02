44ae427b7a0019ea3c249dc9e34809c28b512be1
Emre Akurek <emreyeprem@gmail.com>
2019-04-02 17:35:08 -0500

    practice

--- index.js
+++ index.js
@@ -1416,3 +1416,32 @@ function sumFinder(arr, sum){
   = true
 > sumFinder([6,4,3,2,1,7], 2);
   = false
+
+  // ************ Find the largest sum *********
+  function topSum(arr){
+
+  var biggest = arr[0],
+      second = arr[1],
+      len = arr.length,
+      i = 2;
+
+  if (len<2) return null;
+
+  if (biggest<second){
+    biggest = arr[1];
+    second = arr[0];
+  }
+
+  for(; i<len; i++){
+
+   if(arr[i] > biggest){
+      second = biggest;
+      biggest = arr[i];
+    }
+   else if (arr[i]>second){
+      second = arr[i];
+   }
+
+ }
+ return biggest + second;
+}
