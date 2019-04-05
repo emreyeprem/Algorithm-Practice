28e4537c3f519043ee3d99d7a1d0eb7e9c8b7fbb
Emre Akurek <emreyeprem@gmail.com>
2019-04-05 09:23:32 -0500

    practice

--- index.js
+++ index.js
@@ -1445,3 +1445,16 @@ function sumFinder(arr, sum){
  }
  return biggest + second;
 }
+
+// ********* Counting Zeros **********
+function countZero(n){
+  var count = 0;
+  while(n>0){
+   count += Math.floor(n/10);
+   n = n/10;
+  }
+  return count;
+}
+> countZero(2014);
+  = 223
+// ***************************
