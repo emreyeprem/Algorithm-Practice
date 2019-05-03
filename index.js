4bb2fd58e791e93d9c8ec249351ed92c0c05247c
Emre Akurek <emreyeprem@gmail.com>
2019-05-03 10:19:31 -0500

    practice

--- index.js
+++ index.js
@@ -249,6 +249,11 @@ function String(string){
     return 'not ' + string;
   }
 }
+// ========= how to compare two arrays in javascript ========
+var a1 = [1,2,3];
+var a2 = [1,2,3];
+console.log(a1==a2);    // Returns false
+console.log(JSON.stringify(a1)==JSON.stringify(a2));    // Returns true 
 
 // -------- Combining .map(), .reduce(), and .filter() ---------
 var personnel = [
