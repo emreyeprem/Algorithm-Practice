b18e901aa1817ef3c68e8024f7dcb5898ad35e1b
Emre Akurek <emreyeprem@gmail.com>
2019-05-12 21:39:58 -0500

    practice

--- index.js
+++ index.js
@@ -627,6 +627,18 @@ function numOfSubbarrays(arr) {
 	return c;
 }
 
+// ==================== Factorize a Number ================
+
+function factorize(num) {
+	let factors = []
+	for (i = 1; i <= num; i++) {
+		if (num % i === 0) {
+			factors.push(i)
+		}
+	}
+	return factors
+}
+
 
 // -----------------------  altPairs ---------------------
 
